3d2fcaad4bd3e16edaf563845e195fdbdb057428
aesrubab <rubabhuseynova013@gmail.com>
2024-09-22 00:20:57 +0400

    Add project files.

---
+++ ConsoleApp22/AppDbContext/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿
+
+using ConsoleApp22.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ConsoleApp22.AppDbContext;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=WINDOWS-2P363HE;Initial Catalog=shop;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+            .HasMany(c => c.Products)
+            .WithOne(p => p.Category)
+            .HasForeignKey(p => p.CategoryId);
+    }
+}
---
+++ ConsoleApp22/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace ConsoleApp22.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string CategoryName { get; set; }
+    public List<Product> Products { get; set; }
+}
